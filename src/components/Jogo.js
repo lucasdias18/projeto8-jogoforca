6ef52a3209c6a2f210a9188928d8fa35e5f0652b
Lucas Dias <lucasdias.ld10@gmail.com>
2022-11-28 17:53:15 -0300

    versão 2

---
+++ src/components/Jogo.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+export default Jogo() {
+
+    return (
+        <Forca>
+            <img src='./assets/forca0.png' />
+        </Forca>
+    )
+}
+
+const Forca = styled.div`
+height: 467px;
+width: 400px;`
--- src/components/Letras.js
+++ src/components/Letras.js
@@ -4,9 +4,13 @@ const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m
 // alfabeto.map((letra) => )
 export default function Letras() {
     // const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-    // let botoes;
-    let i;
-    for (i=0; i<alfabeto.length; i++) {
+    let botoes;
+    // return (
+    //     <>
+    //         <Botao />
+    //     </>
+    // )
+    for (let i=0; i<alfabeto.length; i++) {
         // botoes += <Botao>{alfabeto[i]}</Botao>
         return Botao(i)
     }
@@ -15,6 +19,7 @@ export default function Letras() {
 }
 
 function Botao(i) {
+    // console.log(alfabeto)
     return (
         <button>{alfabeto[i]}</button>
     )
